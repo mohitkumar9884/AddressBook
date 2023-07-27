5e59a40daa689797a6801fb388cf715ddb28979c
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-27 16:41:18 +0530

    [mohitkumar9884] creating/adding branch and program ContactUC1

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public static Contact Personal()
+        {
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            return new Contact
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+                PhoneNumber = phoneNumber,
+                Email = email
+            };
+        }
+
+
+
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to addressbook program.");
+            Console.WriteLine("Welcome to AddressBook program.");
+            Contact contact = Contact.Personal();
+
+            Console.WriteLine("\nContact Details:");
+            Console.WriteLine("First Name: " + contact.FirstName);
+            Console.WriteLine("Last Name: " + contact.LastName);
+            Console.WriteLine("Address: " + contact.Address);
+            Console.WriteLine("City: " + contact.City);
+            Console.WriteLine("State: " + contact.State);
+            Console.WriteLine("Zip: " + contact.Zip);
+            Console.WriteLine("Phone Number: " + contact.PhoneNumber);
+            Console.WriteLine("Email: " + contact.Email);
         }
     }
 }
