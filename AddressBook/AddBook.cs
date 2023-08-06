683509aeffa87b901515f26298f522ddb33b04fc
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-08-07 00:46:57 +0530

    [mohitkumar9884] adding/creating Branch and Program AddAddressBook

---
+++ AddressBook/AddBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddBook
+    {
+        public string Name { get; set; }
+        public List<Contact> Contacts { get; } = new List<Contact>();
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,9 +1,10 @@
-﻿namespace AddressBook
+﻿using System.Collections.Generic;
+
+namespace AddressBook
 {
     public class Program
     {
-
-        static List<Contact> addressbook = new List<Contact>();
+        static Dictionary<string, AddBook> addressbooks = new Dictionary<string, AddBook>();
 
         static void Main(string[] args)
         {
@@ -12,11 +13,13 @@
             while (true)
             {
                 Console.WriteLine("\nSelect an option:");
-                Console.WriteLine("1. Add a new contact");
-                Console.WriteLine("2. View all contacts");
-                Console.WriteLine("3. Edit an existing contact");
-                Console.WriteLine("4. Delete a contact");
-                Console.WriteLine("5. Exit");
+                Console.WriteLine("1. Add a new Address Book");
+                Console.WriteLine("2. Select an Address Book");
+                Console.WriteLine("3. Add a new contact");
+                Console.WriteLine("4. View all contacts");
+                Console.WriteLine("5. Edit an existing contact");
+                Console.WriteLine("6. Delete a contact");
+                Console.WriteLine("7. Exit");
                 Console.WriteLine(" ");
 
                 if (int.TryParse(Console.ReadLine(), out int choice))
@@ -24,21 +27,24 @@
                     switch (choice)
                     {
                         case 1:
-                            AddExtraContact();
+                            AddNewAddressBook();
                             break;
                         case 2:
-                            AddNewContact();
+                            SelectAddressBook();
                             break;
                         case 3:
-                            ViewAllContacts();
+                            AddNewContact();
                             break;
                         case 4:
-                            EditContact();
+                            ViewAllContacts();
                             break;
                         case 5:
-                            DeleteContact();
+                            EditContact();
                             break;
                         case 6:
+                            DeleteContact();
+                            break;
+                        case 7:
                             Environment.Exit(0);
                             break;
                         default:
@@ -53,99 +59,100 @@
             }
         }
 
-        static void AddExtraContact()
+        static void AddNewAddressBook()
         {
-            while (true)
+            Console.Write("Enter the name of the new Address Book: ");
+            string name = Console.ReadLine();
+
+            if (addressbooks.ContainsKey(name))
             {
-                Contact newContact = Contact.Personal();
-
-                addressbook.Add(newContact);
-                Console.WriteLine("New contact added successfully!");
-
-                Console.Write("Do you want to add another contact? (y/n): ");
-                string response = Console.ReadLine();
-                if (response.ToLower() != "y")
-                {
-                    break;
-                }
+                Console.WriteLine($"Address Book '{name}' already exists.");
+            }
+            else
+            {
+                addressbooks[name] = new AddBook { Name = name };
+                Console.WriteLine($"Address Book '{name}' created successfully!");
             }
         }
 
+        static void SelectAddressBook()
+        {
+            Console.Write("Enter the name of the Address Book you want to select: ");
+            string name = Console.ReadLine();
+
+            if (addressbooks.TryGetValue(name, out AddBook selectedAddressBook))
+            {
+                Console.WriteLine($"Selected Address Book: {selectedAddressBook.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"Address Book '{name}' does not exist.");
+            }
+        }
 
         static void AddNewContact()
         {
-                Console.WriteLine("Enter Contact Details:");
-                Console.Write("First Name: ");
-                string firstName = Console.ReadLine();
+            Console.Write("Enter the name of the Address Book where you want to add a new contact: ");
+            string name = Console.ReadLine();
 
-                Console.Write("Last Name: ");
-                string lastName = Console.ReadLine();
-
-                Console.Write("Address: ");
-                string address = Console.ReadLine();
-
-                Console.Write("City: ");
-                string city = Console.ReadLine();
-
-                Console.Write("State: ");
-                string state = Console.ReadLine();
-
-                Console.Write("Zipcode: ");
-                string zip = Console.ReadLine();
-
-                Console.Write("Phone Number: ");
-                string phoneNumber = Console.ReadLine();
-
-                Console.Write("Email: ");
-                string email = Console.ReadLine();
-
-                Contact newContact = new Contact
-                {
-                    FirstName = firstName,
-                    LastName = lastName,
-                    Address = address,
-                    City = city,
-                    State = state,
-                    Zipcode = zip,
-                    PhoneNumber = phoneNumber,
-                    EmailAddress = email
-                };
-
-                addressbook.Add(newContact);
-                Console.WriteLine("New contact added successfully!");
-        }
-            static void ViewAllContacts()
+            if (addressbooks.TryGetValue(name, out AddBook selectedAddressBook))
             {
-                if (addressbook.Count == 0)
+                Contact newContact = Contact.Personal();
+
+                selectedAddressBook.Contacts.Add(newContact);
+                Console.WriteLine("New contact added successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"Address Book '{name}' does not exist.");
+            }
+        }
+
+        static void ViewAllContacts()
+        {
+            Console.Write("Enter the name of the Address Book you want to view contacts: ");
+            string name = Console.ReadLine();
+
+            if (addressbooks.TryGetValue(name, out AddBook selectedAddressBook))
+            {
+                if (selectedAddressBook.Contacts.Count == 0)
                 {
-                    Console.WriteLine("Address book is empty.");
+                    Console.WriteLine($"Address book '{name}' is empty.");
                 }
                 else
                 {
-                    Console.WriteLine("\nContact Details:");
-                    foreach (var contact in addressbook)
+                    Console.WriteLine($"\nContacts in Address Book '{name}':");
+                    foreach (var contact in selectedAddressBook.Contacts)
                     {
-                        Console.WriteLine("First Name: " + contact.FirstName);
-                        Console.WriteLine("Last Name: " + contact.LastName);
-                        Console.WriteLine("Address: " + contact.Address);
-                        Console.WriteLine("City: " + contact.City);
-                        Console.WriteLine("State: " + contact.State);
-                        Console.WriteLine("Zip: " + contact.Zipcode);
-                        Console.WriteLine("Phone Number: " + contact.PhoneNumber);
-                        Console.WriteLine("Email: " + contact.EmailAddress);
+                        Console.WriteLine($"First Name: {contact.FirstName}");
+                        Console.WriteLine($"Last Name: {contact.LastName}");
+                        Console.WriteLine($"Address: {contact.Address}");
+                        Console.WriteLine($"City: {contact.City}");
+                        Console.WriteLine($"State: {contact.State}");
+                        Console.WriteLine($"Zip: {contact.Zipcode}");
+                        Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                        Console.WriteLine($"Email: {contact.EmailAddress}");
                         Console.WriteLine("---------------------------------");
                     }
                 }
-
-
             }
-            static void EditContact()
+            else
+            {
+                Console.WriteLine($"Address Book '{name}' does not exist.");
+            }
+        }
+        static void EditContact()
+        {
+            Console.Write("Enter the name of the Address Book containing the contact you want to edit: ");
+            string bookName = Console.ReadLine();
+
+            if (addressbooks.TryGetValue(bookName, out AddBook selectedAddressBook))
             {
                 Console.Write("Enter the First Name of the contact you want to edit: ");
                 string firstName = Console.ReadLine();
 
                 Contact contactToEdit = null;
-                foreach (Contact contact in addressbook)
+                foreach (Contact contact in selectedAddressBook.Contacts)
                 {
                     if (contact.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase))
                     {
@@ -187,12 +194,30 @@
 
                 Console.WriteLine("Contact details updated successfully!");
             }
-            static void DeleteContact()
+            else
+            {
+                Console.WriteLine($"Address Book '{bookName}' does not exist.");
+            }
+        }
+        static void DeleteContact()
+        {
+            Console.Write("Enter the name of the Address Book containing the contact you want to delete: ");
+            string bookName = Console.ReadLine();
+
+            if (addressbooks.TryGetValue(bookName, out AddBook selectedAddressBook))
             {
                 Console.Write("Enter the First Name of the contact you want to delete: ");
                 string firstName = Console.ReadLine();
 
-                Contact contactToDelete = addressbook.Find(c => c.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase));
+                Contact contactToDelete = null;
+                foreach (Contact contact in selectedAddressBook.Contacts)
+                {
+                    if (contact.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        contactToDelete = contact;
+                        break;
+                    }
+                }
 
                 if (contactToDelete == null)
                 {
@@ -200,10 +225,15 @@
                     return;
                 }
 
-                addressbook.Remove(contactToDelete);
+                selectedAddressBook.Contacts.Remove(contactToDelete);
                 Console.WriteLine("Contact deleted successfully!");
             }
-        
+            else
+            {
+                Console.WriteLine($"Address Book '{bookName}' does not exist.");
+            }
+        }
+
 
     }
 
